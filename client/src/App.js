061415408499ce894e0d1768b3f8385b91392d34
ianzyvith <izyvith25@gmail.com>
2022-10-06 18:08:08 -0400

    done

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,11 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
@@ -20,8 +22,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -32,43 +44,33 @@ function App() {
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
-  <Routes>
-    <Route
-      path="/"
-      element={<Home />}
-    />
-    <Route
-      path="/login"
-      element={<Login />}
-    />
-    <Route
-      path="/signup"
-      element={<Signup />}
-    />
-    <Route
-      path="/profile"
-      element={<Profile />}
-    />
-    <Route
-      path="/thought"
-      element={<SingleThought />}
-    />
-
-    <Route
-      path="*"
-      element={<NoMatch />}
-    />
-
-<Route path="/profile">
-    <Route path=":username" element={<Profile />} />
-    <Route path="" element={<Profile />} />
-  </Route>
-  <Route
-    path="/thought/:id"
-    element={<SingleThought />}
-  />
-  </Routes>
-</div>
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
+              />
+              <Route 
+                path="/thought/:id" 
+                element={<SingleThought />} 
+              />
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
+              />
+            </Routes>
+          </div>
           <Footer />
         </div>
       </Router>
